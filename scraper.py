9f340ad4414037ddc46058660e34388e908ed080
Alex Pfeffer <apfeffer@tzpgroup.com>
2026-06-23 15:21:44 -0400

    Remove unmatched name logging (pre-1980 players, expected)

--- scraper.py
+++ scraper.py
@@ -433,12 +433,6 @@ def merge_tiered_teams(players: dict):
             p['allDef2'] = all_def[name]['second']
             def_matched += 1
     logger.info(f'All-NBA: matched {nba_matched}, All-Defensive: matched {def_matched}')
-    unmatched_nba = [n for n in all_nba if n not in player_names]
-    unmatched_def = [n for n in all_def if n not in player_names]
-    if unmatched_nba:
-        logger.info(f'All-NBA unmatched ({len(unmatched_nba)}): {unmatched_nba}')
-    if unmatched_def:
-        logger.info(f'All-Defensive unmatched ({len(unmatched_def)}): {unmatched_def}')
 
 
 def merge_draft(players: dict):
